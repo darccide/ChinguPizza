5176bb9d9dea0770eb6e987ced8c01412a1693bf
darccide <darccide@protonmail.com>
2020-03-12 21:54:16 -0500

    wired up XML and Have rudimentary first page

--- public/app.js
+++ public/app.js
@@ -1,5 +1,70 @@
 // Frontend Logic for application
 
+// Container for frontend application
  var app = {};
+console.log("hello!")
+// Config
+app.config = {
+  'sessionToken': false
+};
 
- console.log("Hello Console World!!");
+// AJAX Client (for RESTful API)
+app.client = {}
+
+// Interface for making API calls
+app.client.request = function(headers, path, method, queryStringObject, payload, callback) {
+
+  // Set defaults
+  headers = typeof(headers) == 'object' && headers !== null ? headers : {};
+  path = typeof(path) == 'string' ? path : '/';
+  method = typeof(method) == 'string' && ['POST', 'GET', 'PUT', 'DELETE']
+    .indexOf(method.toUpperCase()) > -1 ? method.toUpperCase() : 'GET';
+  queryStringObject = typeof(queryStringObject) == 'object'
+    && queryStringObject !== null ? queryStringObject : {};
+  payload = typeof(payload) == 'object' && payload !== null ? payload : {};
+  callback = typeof(callback) == 'function' ? callback : false;
+
+  // For each query string parameter sent, add it to the path
+  let requestUrl = path + '?';
+  let counter = 0;
+
+  for (let queryKey in queryStringObject) {
+    if (queryStringObject.hasOwnProperty(queryKey)) {
+      counter++;
+      // If at least one query string parameter has already been added,
+      // prepend new ones with an ampersand
+      if (counter > 1) {
+        requestUrl += '&';
+      }
+      // Add the key and value
+      requestUrl += queryKey + '=' + queryStringObject[queryKey];
+    }
+  }
+
+  // If there is a current session token set, add that as a header
+  if (app.config.sessionToken) {
+    xhr.setRequestHeader('token', app.config.sessionToken.id);
+  }
+
+  // When the request comes back, handle the response
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      let statusCode = xhr.status;
+      let responseReturned = xhr.responseText;
+
+      // Callback if requested,
+      if (callback) {
+        try {
+          let parsedResponse = JSON.parse(responseReturned);
+          callback(statusCode, parsedResponse);
+        } catch (e) {
+          callback(statusCode, false);
+        }
+      }
+    }
+  }
+
+  // Send the payload as JSON
+  let payloadString = JSON.stringify(payload);
+  xhr.send(payloadString);
+};
