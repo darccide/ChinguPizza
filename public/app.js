74c7593918e2f4ea0d1accd62d5f418407936671
darccide <darccide@protonmail.com>
2020-03-14 23:43:13 -0500

    sign up frontend logic nearly completed

--- public/app.js
+++ public/app.js
@@ -1,8 +1,8 @@
 // Frontend Logic for application
 
 // Container for frontend application
- var app = {};
-console.log("hello!")
+let app = {};
+
 // Config
 app.config = {
   'sessionToken': false
@@ -41,6 +41,18 @@ app.client.request = function(headers, path, method, queryStringObject, payload,
     }
   }
 
+  // Form the http request as a JSON type
+  let xhr. new XMLHttpRequest();
+  xhr.open(method, requestUrl, true);
+  xhr.setRequestHeader("Content-type", "application/json");
+
+  // For each header sent, add it to the request
+  for (let headerKey in headers) {
+    if (headers.hasOwnProperty(headerKey)) {
+      xhr.setRequestHeader(headerKey, headers[headerKey]);
+    }
+  }
+
   // If there is a current session token set, add that as a header
   if (app.config.sessionToken) {
     xhr.setRequestHeader('token', app.config.sessionToken.id);
@@ -67,4 +79,70 @@ app.client.request = function(headers, path, method, queryStringObject, payload,
   // Send the payload as JSON
   let payloadString = JSON.stringify(payload);
   xhr.send(payloadString);
+};
+
+
+// Bind the forms
+app.bindForms = function() {
+  document.querySelector("form").addEventListener("submit", function(e) {
+    // Stop it from submitting
+    e.preventDefault();
+    let formId = this.id;
+    let path = this.action;
+    let method = this.method.toUpperCase();
+
+    // Hide the error message (if it's currently shown due to a previous error)
+    document.querySelector("#" + formId + " .formError").style.display = 'hidden';
+
+    // Turn the inputs into a payload
+    let payload = {};
+    let elements = this.elements;
+    for (let i = 0; i < elements.length; i++) {
+      if (elements[i].type !== 'submit') {
+        let valueOfElement = elements[i].type == 'checkbox'
+        ? elements[i].checked : elements[i].value;
+
+        payload[elements[i].name] = valueOfElement;
+      }
+    }
+
+    // Call the API
+    app.client.request(undefined, path, method, undefined, payload, function(statusCode, responsePayload) {
+      // Display an error on the form if needed
+      if (statusCode !== 200) {
+
+        // Try to get the error from the api, or set a default error message
+        let error = typeof(responsePayload.Error) == 'string'
+        ? responsePayload.Error : 'An Error has occured, please try again';
+
+        // Set the formError field with the error text
+        document.querySelector("#" + formId + " .formError").innerHTML = error;
+
+        // Show (unhide) the form error field on the form
+        document.querySelector("#" + formId + " .formError").style.display = 'block';
+      } else {
+        // If successful, send to form response processor
+        app.formResponseProcessor(formId, payload, responsePayload);
+      }
+    });
+  });
+};
+
+// Form response processor
+app.formResponseProcessor = function(formId, requestPayload, responsePayload) {
+  let functionToCall = false;
+  if (formId == 'accountCreate') {
+    // @TODO Do something here now that the accoutn has been created successfully
+  }
+};
+
+// Init (bootstrapping)
+app.init = function() {
+  // Bind all the form submissions
+  app.bindForms();
+};
+
+// Call the init processes after the window loads
+window.onload = function() {
+  app.init();
 };
